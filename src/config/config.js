798876b455c2fb3e0a8bae0c37e138b262c734c2
Rehana Abdulber <rabdulber@excellerentsolutions.com>
2021-12-21 12:14:10 +0300

    minor frontend changes

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    server: 'http://localhost:9000',
-    host: 'http://localhost:9000/api/',
+    server: 'http://192.168.0.103:9000',
+    host: 'http://192.168.0.103:9000/api/',
     file: 'http://',
     mapApi: 'AIzaSyA9KpdQqHo-73vCPqEuGAMfdP38KM-7Eac',
     fb_Rss: 'https://rss.app/feeds/8wmepjtuzAugFZVs.json',
