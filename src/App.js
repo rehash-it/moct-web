22c82ce0b011f92448858706bd6a8e868f88562e
Ezira Ashenafi <eazash22@gmail.com>
2022-07-18 17:57:01 +0300

    update global theme

--- src/App.js
+++ src/App.js
@@ -26,46 +26,55 @@ import Forums from './components/pages/Forums'
 import ClosedUserForum from './components/Forum/ClosedUserForum'
 import UserForum from './components/Forum/UserForum'
 import { MainLayout } from './components/layouts/mainLayout'
+import { createTheme, ThemeProvider } from '@material-ui/core'
 export default function App() {
   const { t, i18n } = useTranslation();
   const [news, dispatchNews] = useReducer(newsReducer, newsState)
   const [socket, setSocket] = useState('')
 
+  const theme = createTheme({
+    palette: {
+      primary: {main : "#0053af"},
+      secondary:{main:  "#f50057"}
+    }
+  })
   /**set socket */
   useEffect(() => setSocket(webSocket), [])
   return (
-    <StoreContext.Provider value={{ news, dispatchNews }}>
-      <LanguageContext.Provider value={{ t, i18n }}>
-        <SocketContext.Provider value={{ socket, setSocket }}>
-          <BrowserRouter>
-            <Chat />
-            <MainLayout>
-              <Switch>
-                <Route exact path='/'><Home /></Route>
-                <Route path='/news/:id'><NewDetail /></Route>
-                <Route path='/news'><News /></Route>
-                <Route path='/about'><About /></Route>
-                <Route path='/history'><History /></Route>
-                <Route path='/docs/:id'><ResearchDetails /></Route>
-                <Route path='/sites'><Sites /></Route>
-                <Route path='/docs'><Docs /></Route>
-                <Route path='/messageOfMoct'><MessageMoct /></Route>
-                <Route path='/vacancy'><Vacancy /></Route>
-                <Route path='/admin'><Dashboard /></Route>
-                <Route path='/bids'><Bids /></Route>
-                <Route path='/login'><Login /></Route>
-                <Route path='/site/:id'><SiteDetails /></Route>
-                <Route path='/search/:index'><Search /> </Route>
-                <Route path='/chart'><OrganizationChart /></Route>
-                <Route path='/forum/:id'><UserForum /></Route>
-                <Route path='/forums'><Forums /> </Route>
-                <Route path='/closedForums'><ClosedUserForum /></Route>
-                <Route><NotFound /></Route>
-              </Switch>
-            </MainLayout>
-          </BrowserRouter>
-        </SocketContext.Provider>
-      </LanguageContext.Provider>
-    </StoreContext.Provider>
+    <ThemeProvider theme={theme}>
+      <StoreContext.Provider value={{ news, dispatchNews }}>
+        <LanguageContext.Provider value={{ t, i18n }}>
+          <SocketContext.Provider value={{ socket, setSocket }}>
+            <BrowserRouter>
+              <Chat />
+              <MainLayout>
+                <Switch>
+                  <Route exact path='/'><Home /></Route>
+                  <Route path='/news/:id'><NewDetail /></Route>
+                  <Route path='/news'><News /></Route>
+                  <Route path='/about'><About /></Route>
+                  <Route path='/history'><History /></Route>
+                  <Route path='/docs/:id'><ResearchDetails /></Route>
+                  <Route path='/sites'><Sites /></Route>
+                  <Route path='/docs'><Docs /></Route>
+                  <Route path='/messageOfMoct'><MessageMoct /></Route>
+                  <Route path='/vacancy'><Vacancy /></Route>
+                  <Route path='/admin'><Dashboard /></Route>
+                  <Route path='/bids'><Bids /></Route>
+                  <Route path='/login'><Login /></Route>
+                  <Route path='/site/:id'><SiteDetails /></Route>
+                  <Route path='/search/:index'><Search /> </Route>
+                  <Route path='/chart'><OrganizationChart /></Route>
+                  <Route path='/forum/:id'><UserForum /></Route>
+                  <Route path='/forums'><Forums /> </Route>
+                  <Route path='/closedForums'><ClosedUserForum /></Route>
+                  <Route><NotFound /></Route>
+                </Switch>
+              </MainLayout>
+            </BrowserRouter>
+          </SocketContext.Provider>
+        </LanguageContext.Provider>
+      </StoreContext.Provider>
+    </ThemeProvider>
   )
 }
